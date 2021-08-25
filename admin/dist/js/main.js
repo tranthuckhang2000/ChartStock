fd5967f64f571f44a635c21840db93fb64923cd1
tranthuckhang2000 <73299797+tranthuckhang2000@users.noreply.github.com>
2021-08-25 15:06:59 +0700

    update admin

---
+++ admin/dist/js/main.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//     console.log($('#btn-add-account'))
+//     $('#btn-add-account').click(function(){
+//         $('#add-account-user').toggle();
+//     })
+
+// })
