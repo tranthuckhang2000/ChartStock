b93350d40be06eae23977696c81fdb03f874b430
tranthuckhang2000 <73299797+tranthuckhang2000@users.noreply.github.com>
2021-08-27 11:20:57 +0700

    update code menu

--- zonebiz/js/main.js
+++ zonebiz/js/main.js
@@ -1,14 +1,38 @@
 $(document).ready(function(){
+    let status = 0;
     $('#btn-menu-mobile').click(function(){
-        $('.nav-mobile').toggle();
-        $('.nav-overlay').toggle();
+        if(status === 0){
+            changeStatus('0%');
+            status = 1;
+        }
+
     })
-    $('#exit').click(function(){
-        $('.nav-mobile').toggle();
-        $('.nav-overlay').toggle();
+    $('#exit').click(function(){      
+        changeStatus('100%');
+        status = 0;
     })
     $('.nav-overlay').click(function(){
-        $('.nav-mobile').toggle();
-        $('.nav-overlay').toggle();
+        changeStatus('100%');
+        status = 0;
     })
+
+    function changeStatus(value){
+        $('.nav-overlay').toggle();
+        $('.nav-mobile').css({
+        '-webkit-transform' : `translate(${value})`,
+            '-webkit-transition': 'transform linear 0.2s',
+            
+            '-moz-transform' : `translate(${value})`,
+            '-moz-transition': 'transform linear 0.2s',
+
+            '-ms-transform' : `translate(${value})`,
+            '-ms-transition': 'transform linear 0.2s',
+
+            '-o-transform' : `translate(${value})`,
+            'transition': 'transform linear 0.2s',
+
+            '-o-transform' : `translate(${value})`,
+            'transition': 'transform linear 0.2s',
+        })
+    }
 })
